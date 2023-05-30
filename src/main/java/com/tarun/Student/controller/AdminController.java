31ec1e2da4f7b12b46a78e0ce0bfe8e14149bd54
YARISINGU TARUN <118651530+yarisingu@users.noreply.github.com>
2023-05-30 18:28:04 +0000

    minorchanges

--- src/main/java/com/tarun/Student/controller/AdminController.java
+++ src/main/java/com/tarun/Student/controller/AdminController.java
@@ -2,7 +2,6 @@ package com.tarun.Student.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
